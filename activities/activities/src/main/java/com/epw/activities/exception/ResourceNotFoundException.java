5863f6cb8cb0af22bae5a6c2d07e9551c624ada5
Santiago <iemccp7.5enriquezdavid@gmail.com>
2026-03-02 12:53:38 -0500

    Update activities

---
+++ activities/activities/src/main/java/com/epw/activities/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.epw.activities.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        
+        super(message);
+    }
+}
