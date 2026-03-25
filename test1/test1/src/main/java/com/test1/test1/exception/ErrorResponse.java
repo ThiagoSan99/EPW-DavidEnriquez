742efad7e4b79146196e43d54ad614b187d67d20
Santiago <iemccp7.5enriquezdavid@gmail.com>
2026-03-25 08:43:35 -0500

    fourth

---
+++ test1/test1/src/main/java/com/test1/test1/exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.test1.test1.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private List<String> errors;
+
+    public ErrorResponse(LocalDateTime timestamp, int status, List<String> errors) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.errors = errors;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
---
+++ test1/test1/src/main/java/com/test1/test1/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.test1.test1.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(
+                new ErrorResponse(LocalDateTime.now(), 400, errors),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
